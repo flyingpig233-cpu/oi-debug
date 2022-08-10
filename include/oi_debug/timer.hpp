7d01f23c8dad2327d5f59c8623d894229e5fd09e
flyingpig233-cpu <1686079252@qq.com>
2022-08-10 22:59:38 +0800

    改进功能，重构timer

--- include/oi_debug/timer.hpp
+++ include/oi_debug/timer.hpp
@@ -4,53 +4,62 @@
 #include <bits/chrono.h>
 #include <chrono>
 #include <cstdio>
+#include <cxxabi.h>
 #include <functional>
 #include <iostream>
 #include <source_location>
+#include "to_string.hpp"
 
 class Timer {
   private:
-	std::source_location _location;
-	std::chrono::system_clock::time_point _start_time;
+    std::source_location _location;
+    std::chrono::system_clock::time_point _start_time;
 
   public:
-	Timer(const std::source_location location = std::source_location::current())
-		: _location(location), _start_time(std::chrono::system_clock::now()) {}
-	~Timer() {
-		auto end_time = std::chrono::system_clock::now();
-		auto spend_time = std::chrono::duration_cast<std::chrono::microseconds>(
-			end_time - _start_time);
-		printf("It took %ldus to call %s function in %s\n", spend_time,
-			   _location.function_name(), _location.file_name());
-	}
+    Timer(const std::source_location location = std::source_location::current())
+        : _location(location), _start_time(std::chrono::system_clock::now()) {}
+    ~Timer() {
+        auto end_time = std::chrono::system_clock::now();
+        auto spend_time = std::chrono::duration_cast<std::chrono::microseconds>(
+            end_time - _start_time);
+        printf("It took %ldus to call %s function in %s\n", spend_time,
+               _location.function_name(), _location.file_name());
+    }
 };
 
 template <class callable, class... Args>
-void _get_time_with_args(callable f, const char *func_name,
-						 const char *args_name, Args &&...args) {
-	std::function<typename std::result_of<callable(Args...)>::type()> task(
-		std::bind(std::forward<callable>(f), std::forward<Args>(args)...));
+void get_time(callable &&f, Args &&...args) {
+    using return_type = typename std::result_of<callable(Args...)>::type;
+    std::function<return_type(Args...)> func = f;
 
-	auto start_time = std::chrono::system_clock::now();
-	task(std::forward<Args>(args)...);
-	auto end_time = std::chrono::system_clock::now();
-	auto spend_time = std::chrono::duration_cast<std::chrono::microseconds>(
-		end_time - start_time);
-
-	printf("call %s(%s) spent %ldus\n", func_name, args_name, spend_time);
+    auto start_time = std::chrono::system_clock::now();
+    if constexpr (!std::is_same_v<return_type, void>) {
+        auto return_val = func(std::forward<Args&&>(args)...);
+        auto end_time = std::chrono::system_clock::now();
+        auto spend_time = std::chrono::duration_cast<std::chrono::microseconds>(
+            end_time - start_time);
+        int status;
+        char *func_name =
+            abi::__cxa_demangle(typeid(callable).name(), NULL, NULL, &status);
+        if (status == 0) {
+            printf("call %s spent %ldus, return value: %s\n", func_name, spend_time, oi_debug::to_string(return_val).c_str());
+        } else {
+            fprintf(stderr, "Eh...something error");
+        }
+    } else {
+        func(std::forward<Args&&>(args)...);
+        auto end_time = std::chrono::system_clock::now();
+        auto spend_time = std::chrono::duration_cast<std::chrono::microseconds>(
+            end_time - start_time);
+        int status;
+        char *func_name =
+            abi::__cxa_demangle(typeid(callable).name(), NULL, NULL, &status);
+        if (status == 0) {
+            printf("call %s spent () %ldus\n", func_name, spend_time);
+        } else {
+            fprintf(stderr, "Eh...something error");
+        }
+    }
 }
-
-template <class callable> void _get_time(callable f, const char *func_name) {
-	auto start_time = std::chrono::system_clock::now();
-	f();
-	auto end_time = std::chrono::system_clock::now();
-	auto spend_time = std::chrono::duration_cast<std::chrono::microseconds>(
-		end_time - start_time);
-
-	printf("call %s() spent %ldus\n", func_name, spend_time);
-}
-
-#define get_time_with_args(f, args...) _get_time_with_args(f, #f, #args, args)
-#define get_time(f) _get_time(f, #f)
 
 #endif //_BGHELPER_TIMER_H__
