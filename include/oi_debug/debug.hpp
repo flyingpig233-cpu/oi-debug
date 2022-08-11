4275ba36d60daf493ab911b0062366797934fe56
flyingpig233-cpu <1686079252@qq.com>
2022-08-11 13:51:03 +0800

    完善类型名显示，更加人性化

--- include/oi_debug/debug.hpp
+++ include/oi_debug/debug.hpp
@@ -1,6 +1,7 @@
 #ifndef _DEBUG__
 #define _DEBUG__
 
+#include "get_type.hpp"
 #include "to_string.hpp"
 #include <cxxabi.h>
 #include <iostream>
@@ -9,26 +10,13 @@
 #define debug_with_type(var) _debug_with_type((var), (#var))
 #define debug(var) _debug((var), (#var))
 
-template <typename T> std::string type_name() {
-    int status;
-    std::string tname = typeid(T).name();
-    char *demangled_name =
-        abi::__cxa_demangle(tname.c_str(), NULL, NULL, &status);
-    if (status == 0) {
-        tname = demangled_name;
-        std::free(demangled_name);
-    }
-    return tname;
-}
-
 template <typename T> void _debug_with_type(T &t, const char *var_name) {
-    std::cout << type_name<T>() << " " << var_name << " = "
+    std::cout << TypeParseTraits<T>::get_name() << " " << var_name << " = "
               << oi_debug::to_string(t) << std::endl;
 }
 
 template <typename T> void _debug(T &t, const char *var_name) {
-    std::cout << var_name << " = "
-              << oi_debug::to_string(t) << std::endl;
+    std::cout << var_name << " = " << oi_debug::to_string(t) << std::endl;
 }
 
 #endif // _DEBUG__
--- include/oi_debug/test.cpp
+++ include/oi_debug/test.cpp
@@ -2,22 +2,6 @@
 #include <bits/stdc++.h>
 
 int main() {
-
-    std::cout << "=========Without type=========" << std::endl;
-    std::vector<std::string> strs {"Hello", ",", " world!"};
-    debug(strs);
-    std::tuple<int, double, std::vector<std::string>> tup(114514, 3.1415926535897, strs);
-    debug(tup);
-    std::map<std::string, int> m { {"China", 1}, {"America", 2}, {"Rossia", 3} };
-    debug(m);
-    bool ok = true;
-    debug(ok);
-    std::cout << "==============================" << std::endl;
-    std::cout << std::endl;
-    std::cout << "=========With type=========" << std::endl;
-    debug_with_type(strs);
-    debug_with_type(tup);
-    debug_with_type(m);
-    debug_with_type(ok);
-    std::cout << "==============================" << std::endl;
+    std::map<int, int>m;
+    std::cout << TypeParseTraits<std::vector<std::vector<int>>>::get_name() << std::endl;
 }
