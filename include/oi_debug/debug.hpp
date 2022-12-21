c6b9c1d51791001d593b4dc672e311f6e7474c00
CSSDStudioCXY <l15801217702@gmail.com>
2022-12-21 09:03:25 +0800

    Update debug.hpp

--- include/oi_debug/debug.hpp
+++ include/oi_debug/debug.hpp
@@ -8,15 +8,16 @@
 #include <iostream>
 #include <queue>
 
-#define debug_with_type(var) _debug_with_type((var), (#var))
+
 #define debug(var) _debug((var), (#var))
 
-template <typename T> void _debug_with_type(const T &t, const char *var_name) {
-    oi_debug::println(TypeParseTraits<T>::get_name(), " ", var_name, " = ", t);
-}
-
-template <typename T> void _debug(const T &t, const char *var_name) {
-    oi_debug::println(var_name, " = ", t);
+template <typename T> void _Variable_debug(const T &t, const char *var_name, const bool HaveType) {
+    if(HaveType == true){
+        oi_debug::println(TypeParseTraits<T>::get_name(), " ", var_name, " = ", t);
+    }
+    else if(HaveType == false){
+        oi_debug::println(var_name, " = ", t);
+    }
 }
 
 #endif // _DEBUG__
